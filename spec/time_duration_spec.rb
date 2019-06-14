34d91712402a970f5a204b3c30d69e967de1255c
Daiki Matoba <telnetstat@gmail.com>
2019-06-14 23:45:39 +0900

    Add tests

--- spec/time_duration_spec.rb
+++ spec/time_duration_spec.rb
@@ -1,4 +1,16 @@
 RSpec.describe TimeDuration do
+  describe :parse do
+    it '1:10 => 1:10' do
+      duration = TimeDuration.parse('1:10')
+      expect(duration.to_s).to eq '1:10'
+    end
+
+    it '1:70 => 2:10' do
+      duration = TimeDuration.parse('1:70')
+      expect(duration.to_s).to eq '2:10'
+    end
+  end
+
   describe :+ do
     it '0:40 + 0:30 = 1:10' do
       duration = TimeDuration.parse('0:40') + TimeDuration.parse('0:30')
@@ -8,6 +20,28 @@ RSpec.describe TimeDuration do
     it '50:00 + 50:00 = 100:00' do
       duration = TimeDuration.parse('50:00') + TimeDuration.parse('50:00')
       expect(duration.to_s).to eq '100:00'
+    end
+  end
+
+  describe :- do
+    it '0:40 - 0:30 = 0:10' do
+      duration = TimeDuration.parse('0:40') - TimeDuration.parse('0:30')
+      expect(duration.to_s).to eq '0:10'
+    end
+
+    it '1:00 - 0:20 = 0:40' do
+      duration = TimeDuration.parse('1:00') - TimeDuration.parse('0:20')
+      expect(duration.to_s).to eq '0:40'
+    end
+  end
+
+  describe :<=> do
+    it '0:40 <=> 0:30 => 1' do
+      expect(TimeDuration.parse('0:40') <=> TimeDuration.parse('0:30')).to be 1
+    end
+
+    it '0:30 <=> 0:40 => -1' do
+      expect(TimeDuration.parse('0:30') <=> TimeDuration.parse('0:40')).to be -1
     end
   end
 end
